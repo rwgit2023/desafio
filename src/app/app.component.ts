36769aa3eb4ccf7bf07399ecee2e380ee537b465
rwgit2023 <ronangit2023@hotmail.com>
2023-07-20 23:33:37 -0300

    terceiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'desafio';
+  anoAtual = new Date().getFullYear()
   alerta(){
     alert ('OIa eu aqui')
   }
 }
+
+
