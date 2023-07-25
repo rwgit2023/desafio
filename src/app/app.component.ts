44b56587836149bd4b3a2eb08a7ad3e92e4805e2
rwgit2023 <ronangit2023@hotmail.com>
2023-07-24 22:13:16 -0300

    Oitavo Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,20 @@ export class AppComponent {
   anoAtual = new Date().getFullYear()
   btn1(){
     alert ('OIa eu aqui')
+    // Aqui irá chamar a tela de Home
   }
   btn2(){
     alert ('OIa eu aqui')
+    // Aqui irá chamar a div de Cadastro
+    
   }
   btn3(){
     alert ('OIa eu aqui')
+    // Aqui irá cadastrar
+
+  }
+  btn4(){
+    alert ('OIa eu aqui')
+    // Aqui irá chamar a div de Acesso
   }
 }
-
-
